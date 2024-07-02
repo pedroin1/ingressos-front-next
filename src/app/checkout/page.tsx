b76c2e99b1fafd597eb5cd6e1887fc6d21c6c3c5
triad-pedro-bastos <pedro.bastos@triadpesquisa.com.br>
2024-07-02 08:59:16 -0300

    Refatortando codigo

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,6 +2,7 @@ import { GetEventById } from "@/actions/get-events";
 import FormPagamento from "@/components/formPagamento";
 
 import { formatDateToBr } from "@/util/Date";
+import { calculateTotalPriceOfTickets } from "@/util/Ticket";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
 
@@ -11,30 +12,20 @@ export const metadata: Metadata = {
 };
 
 export default async function PageCheckOut() {
+  let event;
+  let totalPrice;
   const cookieStore = cookies();
   const eventId = cookieStore.get("eventId")?.value;
   const selectedSpots = JSON.parse(cookieStore.get("spots")?.value || "[]");
   const ticketKind = cookieStore.get("ticketKind")?.value;
 
-  let event;
   if (eventId) {
     event = await GetEventById(eventId);
-  } else {
-    event = undefined;
-  }
-
-  let totalPrice;
-
-  if (ticketKind === "full") {
-    totalPrice = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    }).format(selectedSpots.length * (event ? event.price : 0));
-  } else if (ticketKind === "half") {
-    totalPrice = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    }).format(selectedSpots.length * (event ? event.price : 0 / 2));
+    totalPrice = calculateTotalPriceOfTickets(
+      ticketKind,
+      selectedSpots,
+      event.price
+    );
   }
 
   return (
