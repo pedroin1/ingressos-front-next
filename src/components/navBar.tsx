5807d3088120ea09fc3ea4adfe7b5844efad3230
triad-pedro-bastos <pedro.bastos@triadpesquisa.com.br>
2024-07-02 17:59:30 -0300

    Ajustnado botoes da nav bar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BsFillCartCheckFill } from "react-icons/bs";
-import { FaPlus } from "react-icons/fa6";
+import { FaCartShopping, FaPlus } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
 
 export default function NavBar() {
@@ -9,7 +9,7 @@ export default function NavBar() {
     <div className="flex max-w-full items-center justify-items-stretch rounded-2xl bg-bar px-6 py-4 shadow">
       <div className="flex grow items-center justify-between">
         <Link
-          className="flex items-center gap-2 hover:backdrop-brightness-200 rounded-2xl p-2"
+          className="flex items-center gap-2 hover:bg-secondary rounded-xl p-2"
           href={"/event/criarEvento"}
         >
           <FaPlus />
@@ -28,8 +28,8 @@ export default function NavBar() {
           href={"/checkout"}
           className="min-h-6 min-w-6 grow-0 items-center"
         >
-          <div className="w-10 h-10 bg-white rounded-[50%] flex justify-center items-center">
-            <BsFillCartCheckFill className="text-black" size={18} />
+          <div className="px-2 py-2 rounded-xl hover:bg-secondary">
+            <FaCartShopping className="text-[#c1c1c1]" size={24} />
           </div>
         </Link>
       </div>
